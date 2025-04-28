cfeac97b092630fb721c3ea44717b034da034cf2
Deathur <matthieu.baelen@gmail.com>
2025-04-28 09:52:37 +0200

    Reorganisation + Retour sur les anciens projets

--- programming/html-css/playground/5-formulaire-html/exercice-3-hard/script.js
+++ programming/html-css/playground/5-formulaire-html/exercice-3-hard/script.js
@@ -5,24 +5,42 @@ function reset() {
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "none";
     document.getElementById("step4").style.display = "none";
+    document.getElementById("Page").innerText = "Etape 1/3";
 }
 function etape1fini() {
     document.getElementById("step1").style.display = "none";
     document.getElementById("step2").style.display = "block";
     document.getElementById("step3").style.display = "none";
     document.getElementById("step4").style.display = "none";
+    document.getElementById("Page").innerText = "Etape 2/3";
 }
 function etape2fini() {
     document.getElementById("step1").style.display = "none";
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "block";
     document.getElementById("step4").style.display = "none";
+    document.getElementById("Page").innerText = "Etape 3/3";
+}
+function etape2retour() {
+    document.getElementById("step1").style.display = "block";
+    document.getElementById("step2").style.display = "none";
+    document.getElementById("step3").style.display = "none";
+    document.getElementById("step4").style.display = "none";
+    document.getElementById("Page").innerText = "Etape 1/3";
 }
 function etape3fini() {
     document.getElementById("step1").style.display = "none";
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "none";
     document.getElementById("step4").style.display = "block";
+    document.getElementById("Page").innerText = "";
     const mail = document.getElementById("email").value;
     document.getElementById("finmail").innerText = "Un email de confirmation à était envoyé à l'adresse : " + mail;
+}
+function etape3retour() {
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("step2").style.display = "block";
+    document.getElementById("step3").style.display = "none";
+    document.getElementById("step4").style.display = "none";
+    document.getElementById("Page").innerText = "Etape 2/3";
 }
