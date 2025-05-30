9a422244947cfee1d7fbc1aaf5a1f31c42eedefd
Deathur <matthieu.baelen@gmail.com>
2025-05-30 10:43:12 +0200

    exo js condition et boucle terminé

--- programming/javascript/exercice/condition-et-boucle/script.js
+++ programming/javascript/exercice/condition-et-boucle/script.js
@@ -1,3 +1,4 @@
+//Exercice 1
 let years = 18;
 let majority;
 function majorité(age) {
@@ -9,3 +10,62 @@ function majorité(age) {
     }
 }
 console.log(majorité(years));
+//Exercice 2
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+console.log("Test");
+//Exercice 3
+let victory;
+let thinking = Math.floor(Math.random() * 100) + 1;
+let input;
+
+function devin() {
+    victory = false;
+    while (victory  == false) {
+        console.log("Entrez un nombre:");
+        input = prompt();
+        if (input > thinking) {
+            console.log("C'est plus petit");
+        }
+        if (input < thinking) {
+            console.log("C'est plus grand !");
+        }
+        if (input == thinking) {
+            console.log("FELICITATION !");
+            victory = true;
+        }
+        if (input == "") {
+            break;
+        }
+    }
+}
+//Exercice 4
+function date() {
+    let input2 = parseInt(prompt());
+    switch (input2) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            console.log("31 jours");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            console.log("30 jours");
+            break;
+        case 2:
+            console.log("29 jours (bissextiles), 28 jours (non bissextiles)");
+            break;
+        default:
+            console.log("Insérez une valeur entre 1 et 12");
+            break;
+    }
+}
