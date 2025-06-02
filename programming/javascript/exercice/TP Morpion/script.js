cc33d438f452a56b2064e9f69db03f2d6c1b2ae8
Deathur <guerre1678@gmail.com>
2025-06-02 09:35:50 +0200

    tp morpion click

--- programming/javascript/exercice/TP Morpion/script.js
+++ programming/javascript/exercice/TP Morpion/script.js
@@ -1,6 +1,7 @@
-let morpionComplet = 0;
-let tourJoueur = 0;
-let laCase = document.getElementById("case");
+let morpionComplet = false;
+let gameStarting = false;
+let tourJoueur;
+let laCase = document.getElementsByClassName('case1');
 console.log(laCase);
 let caseActuel;
 let joueur = document.getElementsByClassName("joueur");
@@ -10,13 +11,34 @@ function reinitialisation() {
 
 }
 function gameStart() {
-    joueur.innerText = 'Tour du joueur 2';
-    if (tourJoueur == 0) {
-        laCase.addEventListener('click', () => {
-            laCase.style.backgroundImage = "";
-        })
-    }
-    else {
-        
-    };
+    gameStarting = true;
+    tourJoueur = 0;
+    console.log("Game start !");
+    
 }
+function clicking() {
+    console.log(gameStarting);
+            document.addEventListener('click', (e) => {
+                let elementClass = e.target.className;
+                if (elementClass == 'case') {
+                    console.log(elementClass);
+                    let caseActuel = document.getElementById(e.target.id);
+                    console.log(caseActuel);
+                    if (tourJoueur == "0") {
+                        caseActuel.style.backgroundColor = "red";
+                        console.log('Joueur rouge');
+                        tourJoueur = 1;
+                        console.log(tourJoueur);
+                    }
+                    else {
+                        caseActuel.style.backgroundColor = "black";
+                        console.log('Joueur noir');
+                        tourJoueur = 0;
+                        console.log(tourJoueur);
+                    }
+                }
+            });
+    }
+function verification() {
+
+};
