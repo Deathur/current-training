bb2e212bfe003af96abab2ec94ea2a4d0b463596
Deathur <matthieu.baelen@gmail.com>
2025-06-12 11:28:27 +0200

    exo supp tableau

---
+++ programming/javascript/exercice/supplementaire-tableau/script.js
@@ -0,0 +1,15 @@
+const nombres = [1, 5, 12, 8, 130, 44];
+
+nombres.forEach((element) => {
+    console.log(`Le nombre est ${element}`);
+})
+
+const grandsNombres = nombres.filter((element) => {
+    return element > 10;
+})
+console.log(grandsNombres);
+
+const doubles = nombres.map((element) => {
+    return element * 2;
+})
+console.log(doubles);
