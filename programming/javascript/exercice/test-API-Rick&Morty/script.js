f65a1d82a3a420e80975bbd23b2c817318422346
Deathur <matthieu.baelen@gmail.com>
2025-06-19 14:13:35 +0200

    js test api

---
+++ programming/javascript/exercice/test-API-Rick&Morty/script.js
@@ -0,0 +1,28 @@
+let donnees;
+let valeur;
+
+const grille = document.querySelector('.main-grid');
+fetch (`https://rickandmortyapi.com/api/character`)
+            .then ((response)=>response.json())
+            .then((value)=>{
+                console.log(value);
+                
+                for (let p = 1; p <= value.info.pages; p++) {
+                fetch (`https://rickandmortyapi.com/api/character?page=${p}`)
+                    .then ((response)=>response.json())
+                    .then((value)=>{test(value)})
+}
+            })
+
+
+function test(valeur) {
+    for (i = 0; i < valeur.results.length ; i++) {
+        const div = document.createElement('div');
+        let img = document.createElement('img');
+        let name = valeur.results[i].name;
+        img.src = valeur.results[i].image;
+        div.appendChild(document.createTextNode(name));
+        div.appendChild(img);
+        grille.appendChild(div);
+    }
+}
