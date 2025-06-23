d080b11b9ddd2244a51f5e474efafbf0927dbe5d
Deathur <matthieu.baelen@gmail.com>
2025-06-23 08:59:54 +0200

    projet personnel hsr

--- programming/javascript/test-hoyoverse/script.js
+++ programming/javascript/test-hoyoverse/script.js
@@ -1,19 +1,31 @@
 let grille = document.querySelector('.main-grid');
 
-
-fetch("https://vizualabstract.github.io/StarRailStaticAPI/db/fr/characters.json")
+const data = fetch(
+    `https://api.mihomo.me/sr_info_parsed/700731421?lang=fr&is_force_update=true`,
+    {
+      headers: {
+        "User-Agent": "https://review-hsr.vercel.app",
+        Host: "api.mihomo.me",
+      }
+    }
+  )
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    for (i = 0; i <= 9999; i++) {
-      if (data[i] !== undefined) {
-        const div = document.createElement("div");
-        let img = document.createElement("img");
-        let name = data[i].name;
-        img.src = `https://vizualabstract.github.io/StarRailStaticAPI/assets/image/character_preview/${data[i].id}.png`;
-        div.appendChild(document.createTextNode(name));
-        div.appendChild(img);
-        grille.appendChild(div);
+    fetch("https://raw.githubusercontent.com/Mar-7th/StarRailRes/refs/heads/master/index_new/fr/characters.json")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data);
+      for (i = 0; i <= 9999; i++) {
+        if (data[i] !== undefined) {
+          const div = document.createElement("div");
+          let img = document.createElement("img");
+          let name = data[i].name;
+          img.src = `https://raw.githubusercontent.com/Mar-7th/StarRailRes/refs/heads/master/${data[i].preview}`;
+          div.appendChild(document.createTextNode(name));
+          div.appendChild(img);
+          grille.appendChild(div);
+        }
       }
-    }
+    })
   });
