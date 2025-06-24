5c2a57203d30220c033b546dc6aeedfc619004aa
Deathur <matthieu.baelen@gmail.com>
2025-06-24 14:12:58 +0200

    js fetch post

--- programming/javascript/exercice/fetch-post/script.js
+++ programming/javascript/exercice/fetch-post/script.js
@@ -3,7 +3,27 @@ const btn = document.querySelector(".buttonSubmit");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const name = document.querySelector(".name");
-  let nameValue = name.value;
-  console.log(nameValue);
+  const lastName = document.querySelector(".lastName");
+  const firstName = document.querySelector(".firstName");
+  const age = document.querySelector(".age");
+  let lastValue = lastName.value;
+  let firstValue = firstName.value;
+  let ageValue = age.value;
+  fetch("https://685a754b9f6ef96111567b88.mockapi.io/nameList", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      lastName: lastValue,
+      firstName: firstValue,
+      age: ageValue,
+    }),
+  })
+    .then((reponse) => reponse.json())
+    .then((data) => {
+      alert(
+        `Vous avez mis dans la BDD le nom ${firstValue} ${lastValue} qui à ${ageValue} ans`
+      );
+    });
 });
